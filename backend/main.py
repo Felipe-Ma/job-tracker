2038524227e4247d5b2a838980fb464d21e8b506
Felipe-Ma710 <f_martinez8@u.pacific.edu>
2025-04-19 02:03:21 -0700

    adding safety c hecks and new route to get all of db

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
-app = FastAPI() # Create an instance of FastAPI
+from database import SessionLocal
+from models import JobApplication
 
-@app.get("/") # Define a route for the root URL
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
 def read_root():
-    return {"message": "Job tracker API is running!"} # Return a JSON response
+    return {"message": "Job Tracker API is running 🚀"}
+
+@app.get("/jobs")
+def get_jobs(db: Session = Depends(get_db)):
+    try:
+        jobs = db.query(JobApplication).all()
+        return jobs
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
