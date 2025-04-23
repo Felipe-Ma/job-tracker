844b03e335f5de073050a0769e14494e14ab374e
Felipe-Ma710 <f_martinez8@u.pacific.edu>
2025-04-23 00:49:50 -0700

    npm initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>🎯 Job Tracker</h1>
+      <p>Welcome! This will show your job applications soon.</p>
+    </div>
+  );
+}
+
+export default App;
+
