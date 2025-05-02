545bce4549266cf970384f6302dba472d4334f8a
Felipe-Ma710 <f_martinez8@u.pacific.edu>
2025-05-01 22:42:50 -0700

    passing delete function and adding a button that links to button to the job card

--- frontend/src/JobCard.js
+++ frontend/src/JobCard.js
@@ -1,10 +1,12 @@
-function JobCard({ job }) {
+function JobCard({ job, onDelete }) {
     return (
         <div style={{border: "1px solid #ccc", padding: "10px", margin: "10px 0" }}>
             <h3>{job.company_name}</h3>
             <p><strong>Role:</strong> {job.job_title}</p>
             <p><strong>Stage:</strong> {job.stage}</p>
             <p><strong>Date Applied:</strong> {job.date_applied}</p>
+
+            <button onClick={() => onDelete(job.id)}>🗑 Delete</button>
         </div>
     );
 }
